4c2ed8870b6e6a329bf7f4fc40fce6aca8f15842
lnpereyra <lukas.gd7@gmail.com>
2020-09-02 00:21:16 -0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            
+            driver.Navigate().GoToUrl("http://www.google.com");
+
+        }
+    }
+}
