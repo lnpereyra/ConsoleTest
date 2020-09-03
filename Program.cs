b60a5f232712715775c98c5ea2e207885e983933
lnpereyra <lukas.gd7@gmail.com>
2020-09-03 13:52:36 -0300

    Cambio de nombre de variable

--- Program.cs
+++ Program.cs
@@ -29,7 +29,7 @@ namespace ConsoleApp2
 
             driver.SwitchTo().ParentFrame();
 
-            var aliado = "pepito";
+            var aliadito = "pepito";
 
             driver.FindElement(By.XPath("")).SendKeys("asgadg");
 
