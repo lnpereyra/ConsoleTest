d623f76f33fb7b445bcbac5de8ffdac01f4214a0
lnpereyra <lukas.gd7@gmail.com>
2020-09-03 14:15:52 -0300

    variable bien nombrada

--- Program.cs
+++ Program.cs
@@ -35,7 +35,7 @@ namespace ConsoleApp2
 
             Console.ReadLine();
 
-            Console.WriteLine(aliado.Length);
+            Console.WriteLine(aliaditos.Length);
 
             var a = Console.ReadLine();
         }
