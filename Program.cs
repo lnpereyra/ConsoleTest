42d7c1408904d035e503604c3984d7cf022be747
lnpereyra <lukas.gd7@gmail.com>
2020-09-03 14:04:32 -0300

    Commit changes

--- Program.cs
+++ Program.cs
@@ -29,7 +29,7 @@ namespace ConsoleApp2
 
             driver.SwitchTo().ParentFrame();
 
-            var aliadito = "pepito";
+            var aliaditos = "pepito";
 
             driver.FindElement(By.XPath("")).SendKeys("asgadg");
 
